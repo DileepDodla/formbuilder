9df364b0caa26842690eb8fcb24dde2dc879598b
Dileep Reddy Dodla <dileepreddy1999@gmail.com>
2021-02-06 22:53:43 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditAppComponent } from './components/edit-app/edit-app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "admin", component: EditAppComponent },
+  { path: "", redirectTo: "/admin", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { DndModule } from 'ngx-drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditAppComponent } from './components/edit-app/edit-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DndModule
   ],
   providers: [],
   bootstrap: [AppComponent]
